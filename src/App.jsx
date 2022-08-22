bc47b5a0eb134adb3b28ae1b402ad46b5aea4b38
anabellampippo <anabellampippo@gmail.com>
2022-08-22 19:49:58 -0300

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import Menu from './components/Menu';
+import  logo from './images/1620.png'
 
 function App() {
   return (
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'menu.css';
+
+
+
+function Menu () {
+    return (
+      <div className='appContenedor'>
+        <ul>
+          <li>Chupetines</li>
+          <li>Gomitas</li>
+          <li>Caramelos</li>
+        </ul>
+      </div>
+
+    );
+}
+
+
+export default Menu;
